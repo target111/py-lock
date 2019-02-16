35b8f1ec10f5c641f29bd85d3a590446c2c9a316
target111 <target111@protonmail.com>
2019-02-16 09:08:20 +0200

    First commit. WIP!

---
+++ lock.py
@@ -0,0 +1,30 @@
+def init_bytes() -> list:
+    bytes = []
+
+    for _ in range(16):
+        bytes.append(0)
+
+    return bytes
+
+
+def page_to_index(page: int) -> int:
+    if page >= 4 and page <= 7:
+        return (7 - page)
+
+    return (23 - page)
+
+
+def list_to_str(l: list) -> str:
+    return "".join([str(item) for item in l])
+
+
+def bin2hex(binary: str) -> hex:
+    return f'{int(binary, 2):0{(len(binary)+3)//4}x}'
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Mifare ultralight lock bytes translator.",
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # parser.add_argument()
