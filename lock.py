6bf6a8e1f48f3c041e44abcf0ad873ceae6e947d
target111 <target111@protonmail.com>
2019-02-17 11:34:48 +0200

    added info command

--- lock.py
+++ lock.py
@@ -17,12 +17,16 @@ def page_to_index(page: int) -> int:
     return (23 - page)
 
 
-def list_to_str(l: list) -> str:
-    return "".join([str(item) for item in l])
+def list2str(l: list, delimiter: str = "") -> str:
+    return delimiter.join([str(item) for item in l])
 
 
 def bin2hex(binary: str) -> hex:
     return f'{int(binary, 2):0{(len(binary)+3)//4}x}'
+
+
+def hex2bin(hex: str) -> str:
+    return (bin(int(hex, 16))[2:]).zfill(len(hex) * 4)
 
 
 def get_pages(interval: str) -> list:
@@ -44,13 +48,31 @@ def get_pages(interval: str) -> list:
     return pages
 
 
-def parse_args() -> argparse.Namespace:
+def get_locked_pages(lock_bytes: list) -> list:
+    locked_pages = []
+
+    for i in range(4):
+        if int(lock_bytes[i]) != 1:
+            continue
+
+        locked_pages.append(7 - i)
+
+    for i in range(8, 16):
+        if int(lock_bytes[i]) != 1:
+            continue
+
+        locked_pages.append(23 - i)
+
+    return locked_pages
+
+
+def parse_args(help: bool = False) -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Mifare ultralight lock bytes translator.",
         formatter_class=argparse.RawDescriptionHelpFormatter)
 
     subparsers = parser.add_subparsers(
-        dest="action", help="Choose an action you wish to perform.")
+        dest="action", help="Choose the action you wish to perform.")
 
     lock_parser = subparsers.add_parser(
         "lock",
@@ -77,8 +99,15 @@ def parse_args() -> argparse.Namespace:
     lock_parser.add_argument(
         "--lock-otp", help="Locks OTP Page.", action="store_true")
 
-    args = parser.parse_args()
-    return args
+    info_parser = subparsers.add_parser(
+        "info", help="Translate lock bytes from hex to human readable output.")
+    info_parser.add_argument("hex", help="Hexadecimal value of lock bytes.")
+
+    if not help:
+        args = parser.parse_args()
+        return args
+
+    parser.print_help()
 
 
 def main(args=None) -> None:
@@ -100,7 +129,29 @@ def main(args=None) -> None:
         if args.block_lock_otp:
             lock_bytes[7] = 1
 
-        print(bin2hex(list_to_str(lock_bytes)))
+        print(bin2hex(list2str(lock_bytes)))
+
+    elif args.action == "info":
+        lock_bytes = list(hex2bin(args.hex))
+        locked_pages = get_locked_pages(lock_bytes)
+
+        if len(locked_pages):
+            print("Locked pages: %s" % list2str(sorted(locked_pages), ", "))
+
+        if int(lock_bytes[4]) == 1:
+            print("OTP page locked.")
+
+        if int(lock_bytes[5]) == 1:
+            print("Lock config for Page[10-15] frozen.")
+
+        if int(lock_bytes[6]) == 1:
+            print("Lock config for Page[4-9] frozen.")
+
+        if int(lock_bytes[7]) == 1:
+            print("Lock config for OTP Page frozen.")
+
+    else:
+        parse_args(help=True)
 
 
 def run() -> None:
